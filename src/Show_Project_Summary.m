a95b7b0fb8c73df03d2f223c59addb38ce6da6d2
Ahmed Elkady <a.elkady@soton.ac.uk>
2021-02-23 14:39:44 +0000

    update

--- src/Show_Project_Summary.m
+++
@@ -1,9 +0,0 @@
-function []=Show_Project_Summary
-clc
-
-[Text]=Get_Project_Summary;
-
-Project_Summary
-ayy=findall(groot, 'Name', 'Project Summary'); %myguide1 is the file name for guide1
-set(findobj(ayy(1),'Tag','SummaryText'),'String',Text);
-drawnow
